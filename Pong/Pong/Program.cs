759071d205753433bbe161148abc8d889eddc158
noshaba <noshaba@rocketmail.com>
2015-03-27 08:46:42 +0100

    Pong initial commit

---
+++ Pong/Pong/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using SFML.Audio;
+using SFML.Graphics;
+using SFML.Window;
+
+namespace Pong 
+{
+    class Program 
+    {
+        static ContextSettings context = new ContextSettings();
+        static RenderWindow window;
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World! :D");
+            window = new RenderWindow(new VideoMode(1024, 768), "Pong!", Styles.Default, context);
+            window.Closed += window_closed;
+            while (window.IsOpen())
+            {
+                window.Clear();
+                window.Display();
+            }
+        }
+
+        static void window_closed(object sender, EventArgs e)
+        {
+            window.Close();
+        }
+    }
+}
