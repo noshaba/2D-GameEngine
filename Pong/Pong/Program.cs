d962b24b675ed5ee12f1f46f2e772126ddaa97bf
noshaba <noshaba@rocketmail.com>
2015-03-27 11:23:31 +0100

    sprite test

--- Pong/Pong/Program.cs
+++ Pong/Pong/Program.cs
@@ -12,19 +12,48 @@ namespace Pong {
         static ContextSettings context = new ContextSettings();
         static RenderWindow window;
 
+        static Texture mouse_tex = new Texture("../Content/Mouse.png");
+        static Sprite mouse_sprite;
+
         static void Main(string[] args) {
+            /*
+             * stopwatch
+             * renderwindow
+             * setverticalsyncenabled
+             * dt = clock.elapsedtimes / (float) stopwatch.frequency
+             * clock.restart
+             * while dt > tick
+             * 
+             */
             Console.WriteLine("Hello World! :D");
             window = new RenderWindow(new VideoMode(1024, 768), "Pong!", Styles.Default, context);
             window.Closed += window_Closed;
             window.KeyReleased += window_KeyReleased;
+            window.MouseMoved += window_MouseMoved;
+            window.MouseEntered += window_MouseEntered;
+            window.MouseLeft += window_MouseLeft;
             window.SetActive(true);
             window.SetFramerateLimit(60);
+            mouse_sprite = new Sprite(mouse_tex);
             while (window.IsOpen()) {
                 window.DispatchEvents();
                 window.Clear();
 
+                mouse_sprite.Draw(window, RenderStates.Default);
                 window.Display();
             }
+        }
+
+        private static void window_MouseMoved(object sender, MouseMoveEventArgs e) {
+            mouse_sprite.Position = new Vector2f(e.X,e.Y);
+        }
+
+        private static void window_MouseLeft(object sender, EventArgs e) {
+            window.SetMouseCursorVisible(true);
+        }
+
+        private static void window_MouseEntered(object sender, EventArgs e) {
+            window.SetMouseCursorVisible(false);
         }
 
         static void window_KeyReleased(object sender, KeyEventArgs e) {
