6cf0eb9491b152e92ba85abcfed9484ac5125981
noshaba <noshaba@rocketmail.com>
2015-03-27 09:58:15 +0100

    movement of window possible

--- Pong/Pong/Program.cs
+++ Pong/Pong/Program.cs
@@ -7,28 +7,35 @@ using SFML.Audio;
 using SFML.Graphics;
 using SFML.Window;
 
-namespace Pong 
-{
-    class Program 
-    {
+namespace Pong {
+    class Program {
         static ContextSettings context = new ContextSettings();
         static RenderWindow window;
 
-        static void Main(string[] args)
-        {
+        static void Main(string[] args) {
             Console.WriteLine("Hello World! :D");
             window = new RenderWindow(new VideoMode(1024, 768), "Pong!", Styles.Default, context);
-            window.Closed += window_closed;
-            while (window.IsOpen())
-            {
+            window.Closed += window_Closed;
+            window.KeyReleased += window_KeyReleased;
+            window.SetActive(true);
+            window.SetFramerateLimit(60);
+            while (window.IsOpen()) {
+                window.DispatchEvents();
                 window.Clear();
+
                 window.Display();
             }
         }
 
-        static void window_closed(object sender, EventArgs e)
-        {
+        static void window_KeyReleased(object sender, KeyEventArgs e) {
+            if (e.Code == Keyboard.Key.Escape) {
+                window.Close();
+            }
+        }
+
+        static void window_Closed(object sender, EventArgs e) {
             window.Close();
         }
+
     }
 }
