81f9a04fc0970c3dc87af351da95ac1f3beeb194
noshaba <noshaba@rocketmail.com>
2015-05-29 00:50:28 +0200

    bezier spline corrections

--- Shoot em Up/Shoot em Up/Maths/EMath.cs
+++ Shoot em Up/Shoot em Up/Maths/EMath.cs
@@ -13,5 +13,9 @@ namespace Maths {
             a = (h - l) * a + l;
             return a;
         }
+        public static IList<T> Clone<T>(this IList<T> listToClone) where T : ICloneable
+        {
+            return listToClone.Select(item => (T)item.Clone()).ToList();
+        }
     }
 }
--- Shoot em Up/Shoot em Up/Maths/Splines/BezierSpline.cs
+++ Shoot em Up/Shoot em Up/Maths/Splines/BezierSpline.cs
@@ -7,26 +7,23 @@ using SFML.System;
 
 namespace Maths
 {
-    class BezierSpline
+    class BezierSpline : Spline
     {
-        private List<Vector2f> subdividedPolygon = new List<Vector2f>();
         private List<Vector2f> leftSubdivision = new List<Vector2f>();
         private List<Vector2f> rightSubdivision = new List<Vector2f>();
         // for Casteljau Algorithm
         private List<Vector2f> tmpSubdivision1 = new List<Vector2f>();
         private List<Vector2f> tmpSubdivision2 = new List<Vector2f>();
-        // bezier curve
-        private Vector2f[] bezierCurve;
 
-        public Vector2f[] BezierSpline(List<Vector2f> anchors, uint depth, float t)
+        public BezierSpline(List<Vector2f> anchors, uint depth, float t)
         {
             BezierSubdivision(anchors, depth, t);
-            return bezierCurve;
         }
 
         private void BezierSubdivision(List<Vector2f> anchors, uint depth, float t)
         {
             uint devided = 0;
+            List<Vector2f> subdividedPolygon = new List<Vector2f>();
 
             if (depth > 0)
             {
@@ -42,27 +39,27 @@ namespace Maths
                 // Casteljau Algorithm
                 while(tmpSubdivision1.Count > 1)
                 {
-                    for(int i = 0; i < tmpSubdivision1.Count; ++i)
+                    for(int i = 0; i < tmpSubdivision1.Count - 1; ++i)
                         tmpSubdivision2.Add((1 - t) * tmpSubdivision1[i] + t * tmpSubdivision1[i + 1]);
 
                     // first and last value of the algorithm are saved
                     leftSubdivision.Add(tmpSubdivision2.First());
                     rightSubdivision.Add(tmpSubdivision2.Last());
 
-                    tmpSubdivision1 = tmpSubdivision2;
+                    tmpSubdivision1 = tmpSubdivision2.ToList();
                     tmpSubdivision2.Clear();
                 }
 
                 for (int i = 0; i < leftSubdivision.Count; ++i)
                     subdividedPolygon.Add(leftSubdivision[i]);
-                for (int i = rightSubdivision.Count - 1; i > -1; i--)
+                for (int i = rightSubdivision.Count - 1; i > -1; --i)
                     subdividedPolygon.Add(rightSubdivision[i]);
 
                 BezierSubdivision(subdividedPolygon, depth - 1, t);
                 ++devided;
             }
 
-            if(devided == depth) bezierCurve = subdividedPolygon.ToArray();
+            if(devided == depth) base.curve = subdividedPolygon.ToArray();
         }
     }
 }
