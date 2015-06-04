b4c2fc5224cbbc9a720847914b41119a525f4e06
ndoege <ndoege@stud.hs-bremen.de>
2015-06-04 18:57:04 +0200

    item position fix

--- Shoot em Up/Shoot em Up/Game/GameObjects/Item.cs
+++ Shoot em Up/Shoot em Up/Game/GameObjects/Item.cs
@@ -10,11 +10,13 @@ namespace Shoot_em_Up
     class Item : GameObject
     {
         public delegate void ActionListener();
+        private Vector2f initPos;
         public ActionListener listener;
         public Item(String texture, ActionListener listener, Vector2f pos)
             : base(new Texture(texture), pos, 1, 0.01f)
         {
             this.listener = listener;
+            this.initPos = pos;
             this.drawable.Texture = new Texture(texture);
             this.rigidBody.DragCoefficient = 2;
         }
@@ -22,6 +24,7 @@ namespace Shoot_em_Up
         public override void Update()
         {
             base.Update();
+            this.rigidBody.COM = this.initPos;
             if (rigidBody.Collision.collision && rigidBody.Collision.obj.Parent is Player)
             {
                 this.display = false;
